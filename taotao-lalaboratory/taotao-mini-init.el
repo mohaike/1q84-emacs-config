78ba12e1409a3cf49f2f6db6144c17c82b4519b8
黄长涛 <huangchangtao@yodo1.com>
2015-08-27 14:16:38 +0800

    更改窗口分割策略，现在分割的时候默认的最大化

--- taotao-lalaboratory/taotao-mini-init.el
+++ taotao-lalaboratory/taotao-mini-init.el
@@ -1,28 +1,2 @@
-(defun taotao-toggle-frame-maximized ()
-  "Toggle maximization state of the selected frame.
-Maximize the selected frame or un-maximize if it is already maximized.
-Respect window manager screen decorations.
-If the frame is in fullscreen mode, don't change its mode,
-just toggle the temporary frame parameter `maximized',
-so the frame will go to the right maximization state
-after disabling fullscreen mode.
-
-Note that with some window managers you may have to set
-`frame-resize-pixelwise' to non-nil in order to make a frame
-appear truly maximized.
-
-See also `toggle-frame-fullscreen'."
-  (interactive)
-  (if (memq (frame-parameter nil 'fullscreen) '(fullscreen fullboth))
-      (modify-frame-parameters
-       nil
-       `((maximized
-	  . ,(unless (eq (frame-parameter nil 'maximized) 'maximized)
-	       'maximized))))
-    (modify-frame-parameters
-     nil
-     `((fullscreen
-	. ,(unless (eq (frame-parameter nil 'fullscreen) 'maximized)
-	     'maximized))))))
 
 (provide 'taotao-mini-init)
