fcf85705ee1acd8e1168c7c0d01a9e0c78545c41
黄长涛 <huangchangtao@yodo1.com>
2015-09-18 15:32:00 +0800

    更新Xcode7之后新的签名方式

--- taotao-origin-init/taotao-xcode-projcet-code/taotao-xcode-projcet-code.el
+++ taotao-origin-init/taotao-xcode-projcet-code/taotao-xcode-projcet-code.el
@@ -20,6 +20,17 @@
   (delete-line)
   (insert-before-markers "\"f0708d0a-d69f-4c9f-8104-d61c3ee8b5eb\";")  ;; 签名
 
+  (re-search-forward "PROVISIONING_PROFILE")
+  (forward-char 3)
+  (delete-line)
+  (insert-before-markers "\"f0708d0a-d69f-4c9f-8104-d61c3ee8b5eb\";")  ;; 签名
+
+  (re-search-forward "PROVISIONING_PROFILE")
+  (forward-char 3)
+  (delete-line)
+  (insert-before-markers "\"f0708d0a-d69f-4c9f-8104-d61c3ee8b5eb\";")  ;; 签名
+
+
   (save-buffer)
   (kill-this-buffer)
   )
@@ -31,6 +42,16 @@
   (interactive)
   ;; 证书文件路径
   (find-file "~/Documents/Project/SM/StickMan/proj.ios_mac/StickMan.xcodeproj/project.pbxproj")
+  (re-search-forward "PROVISIONING_PROFILE")
+  (forward-char 3)
+  (delete-line)
+  (insert-before-markers "\"\";")  ;; 清空签名
+
+  (re-search-forward "PROVISIONING_PROFILE")
+  (forward-char 3)
+  (delete-line)
+  (insert-before-markers "\"\";")  ;; 清空签名
+
   (re-search-forward "PROVISIONING_PROFILE")
   (forward-char 3)
   (delete-line)
