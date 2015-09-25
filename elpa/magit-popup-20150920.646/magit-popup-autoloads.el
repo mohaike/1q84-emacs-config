abd9a0468297131ef196dcf8b1127c372f41d980
黄长涛 <huangchangtao@yodo1.com>
2015-09-25 09:28:17 +0800

    更新插件【magit】【s】【with-editor】

---
+++ elpa/magit-popup-20150920.646/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22019 16264 714396 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
---
+++ elpa/s-20150923.4/s-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "s" "20150923.4" "The long lost Emacs string manipulation library." 'nil :keywords '("strings"))
