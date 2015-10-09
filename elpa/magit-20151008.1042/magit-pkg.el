895d51415f10bef0c29f95fd521415d0f4f1b865
黄长涛 <huangchangtao@yodo1.com>
2015-10-09 10:56:25 +0800

    更新插件magit

--- elpa/magit-20151008.1042/magit-pkg.el
+++ elpa/magit-20151008.1042/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20151006.1359" "A Git porcelain inside Emacs"
+(define-package "magit" "20151008.1042" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20150812")
     (dash "2.11.0")
