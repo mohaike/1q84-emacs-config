1dfdd7078722d4464525a05086668df6438a277e
黄长涛 <huangchangtao@yodo1.com>
2015-10-19 10:11:15 +0800

    hydra更新插件包【names】【anzu】【hydra】【irony】【lua-mode】【magit】【monokai-theme】

--- elpa/monokai-theme-20151007.1658/monokai-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monokai-theme" "20151007.1658" "A fruity color theme for Emacs." 'nil :url "http://github.com/oneKelvinSmith/monokai-emacs")
---
+++ elpa/monokai-theme-20151014.304/monokai-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "monokai-theme" "20151014.304" "A fruity color theme for Emacs." 'nil :url "http://github.com/oneKelvinSmith/monokai-emacs")
