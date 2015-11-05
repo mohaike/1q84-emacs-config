7ddaba00564e60e2b51bc521df1ecf03c0addc63
黄长涛 <huangchangtao@yodo1.com>
2015-11-05 10:30:02 +0800

    修改【ag】快捷键

--- taotao-plugin-settings/taotao-ag-mode.el
+++ taotao-plugin-settings/taotao-ag-mode.el
@@ -2,7 +2,7 @@
 
 (progn
   (require 'ag)
-  (define-key ag-mode-map (kbd "k") '(lambda () (interactive) 
+  (define-key ag-mode-map (kbd "x") '(lambda () (interactive) 
                                      (let (kill-buffer-query-functions) (kill-buffer-and-window)))))
 
 (provide 'taotao-ag-mode)
