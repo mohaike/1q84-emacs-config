8fd9c73e5d390166bc8dbb2bc07572367cfb1758
黄长涛 <huangchangtao@yodo1.com>
2016-01-29 11:21:37 +0800

    添加割绳子项目路径简写, 添加 sudo 文件函数

--- taotao-origin-init/inside-init.el
+++ taotao-origin-init/inside-init.el
@@ -1198,5 +1198,19 @@ This command assumes point is not in a string or comment."
   (interactive)
   (capitalize-region (point) (1+ (point))))
 
+;; 额, sudo file
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
 
 (provide 'inside-init)
