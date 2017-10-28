95fa85005fde8d5476c8c167274eb3ede7b4511f
HuangChangTao <HuangChangTao@yodo1.com>
2017-10-28 09:40:16 +0800

    添加修改20171028

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 忽略警告的设定，调试emacs的时候必须注释掉!!!
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (eval-after-load "warnings" 
     '(setq display-warning-minimum-level :error))
 
@@ -94,3 +101,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/taotao-lalaboratory")
 (require 'taotao-mini-init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
+ '(package-selected-packages
+   (quote
+    (dracula-theme jq-mode web-beautify xah-css-mode youdao-dictionary yaml-mode switch-window sublimity shader-mode seq reveal-in-finder php-mode multiple-cursors monokai-theme minimap magit lua-mode let-alist json-mode irony hydra htmlize hl-anything highlight-symbol glsl-mode ggtags fold-dwim expand-region dash-at-point csharp-mode cmake-mode browse-kill-ring autopair auto-complete anzu ag ace-jump-mode)))
+ '(safe-local-variable-values (quote ((require-final-newline . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit t :background "#0432ff")))))
--- taotao-lalaboratory/taotao-mini-init.el
+++ taotao-lalaboratory/taotao-mini-init.el
@@ -9,11 +9,14 @@
 
 
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/taotao-local-plugin/theme")
+(load-theme 'dracula t)
 
 
 
-
-
+(set-face-foreground 'modeline "white")
+(set-face-background 'modeline "purple")
+(set-face-background 'modeline-inactive "light blue")
 
 
 (provide 'taotao-mini-init)
